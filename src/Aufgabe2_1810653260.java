0e1bf304ff697ee39c8c255185306259a080d702
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-23 20:40:57 +0100

    zahlen gleichsetzen

--- src/Aufgabe2_1810653260.java
+++ src/Aufgabe2_1810653260.java
@@ -3,14 +3,34 @@ import javax.swing.*;
 public class Aufgabe2_1810653260 {
     public static void main (String[] args){
 
-        int zahl1 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein:"));
+        int zahl1 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein:"));// bitten zahl einzugeben
         int zahl2 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein:"));
+        int i=0; //schleife fangt bei 0 an
+        int durch = 0; // durchlauf fangt bei 0 an
+
+        if (zahl1>zahl2){// wenn zahl1 grösser als zahl2 ist, soll
+// while schleife wiederholt so weit bis die bedingung wahr ist
+            while (i < zahl1-zahl2){//zahl2 solange addiert werden bis es gleich wie zahl1 ist und durchläufe werden gezählt
+                zahl2++;
+                durch++;
 
 
-        if (zahl2 == zahl1) {
-            JOptionPane.showMessageDialog(null, "" + zahl1);
+            }
         }
+        else{
+            while (i < zahl2-zahl1) {
+                zahl1++;
+                durch++;
+            }
+        }
+
+        if (zahl1==zahl2){
+            JOptionPane.showMessageDialog(null, "Die zahlen sind" + zahl1 + "\nDurchgang:" + durch);
+
+        }
+
+    }
 
 
     }
-}
+
