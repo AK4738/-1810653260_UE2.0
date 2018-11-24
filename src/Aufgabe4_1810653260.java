458c69c0c0f40b0f21eb9016a406204c913ab45c
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-24 15:14:38 +0100

    While schleife

--- src/Aufgabe4_1810653260.java
+++ src/Aufgabe4_1810653260.java
@@ -4,6 +4,12 @@ public class Aufgabe4_1810653260 {
     public static void main (String[] args){
         for (int i = 1; i < 11; i++){
             System.out.println("Der Schleifenzähler ist:" + i);
+
+        int f =0;
+        while (f < 10){
+            f++;
+            System.out.println("Der Schleifenzähler ist: " + f);
+        }
         }
     }
 }
