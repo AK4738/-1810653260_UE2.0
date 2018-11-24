1dd61b701c8d8dc13f9635da39a04c6d11540c77
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-24 15:11:00 +0100

    Schleifenzähler

---
+++ src/Aufgabe4_1810653260.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Aufgabe4_1810653260 {
+    public static void main (String[] args){
+        for (int i = 1; i < 11; i++){
+            System.out.println("Der Schleifenzähler ist:" + i);
+        }
+    }
+}
