fe8c194905c34ca7993a358ed0e13393aa48afd8
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-26 13:51:30 +0100

    Aufgabe 5 for

--- src/Aufgabe4_1810653260.java
+++ src/Aufgabe4_1810653260.java
@@ -2,17 +2,17 @@ import javax.swing.*;
 
 public class Aufgabe4_1810653260 {
     public static void main (String[] args){
-        for (int i = 1; i < 11; i++){
+        for (int i = 1; i < 11; i++){//wenn man eine Vaiable hat wo der wert inkrementiert wird
             System.out.println("Der Schleifenzähler ist: " + i);
 
         int f =0;
-        while (f < 10){
+        while (f < 10){//wenn man eine Variable hat wo die bedingung erfüllt werden muss
             f++;
             System.out.println("Der Schleifenzähler ist: " + f);
         }
 
         int a=0;
-        do {
+        do {// führt etwas so weit aus bis die abbruchbedingung erfüllt ist
             a++;
             System.out.println("Der Schleifenzähler ist: " + a);
         }
---
+++ src/Aufgabe5_1810653260.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Aufgabe5_1810653260 {
+    public static void main(String[] args){
+
+        forSchleife(5);
+        forSchleife(10);
+
+
+    }
+
+   public static void forSchleife(int a){
+        for (int i = 0; i <= a; i++){
+            System.out.println("Ausgabe = " + i);
+        }
+    }
+
+
+
+}
