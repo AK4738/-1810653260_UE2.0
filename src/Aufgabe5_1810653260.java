76a381b98516b6dde6393ebdd1321bba9337b1b2
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-26 13:53:07 +0100

    Augabe 5 while

--- src/Aufgabe5_1810653260.java
+++ src/Aufgabe5_1810653260.java
@@ -5,6 +5,8 @@ public class Aufgabe5_1810653260 {
 
         forSchleife(5);
         forSchleife(10);
+        whileSchleife(5);
+        whileSchleife(10);
 
 
     }
@@ -12,6 +14,15 @@ public class Aufgabe5_1810653260 {
    public static void forSchleife(int a){
         for (int i = 0; i <= a; i++){
             System.out.println("Ausgabe = " + i);
+        }
+    }
+
+   public static void whileSchleife(int a){
+        int i = 0;
+
+        while(i <= a){
+            System.out.println(i);
+            i += 2;
         }
     }
 
